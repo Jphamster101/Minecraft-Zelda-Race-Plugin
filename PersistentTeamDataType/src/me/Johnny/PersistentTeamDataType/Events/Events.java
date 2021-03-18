248e9f51d3bce4ce9cb5a7c38683ae06e7501149
Johnny <phamj1998@gmail.com>
2021-03-18 17:41:48 -0500

    Refined team assignment system. Worked on cosmetics Scoreboard appearance and player skin logic

--- PersistentTeamDataType/src/me/Johnny/PersistentTeamDataType/Events/Events.java
+++ PersistentTeamDataType/src/me/Johnny/PersistentTeamDataType/Events/Events.java
@@ -1,13 +1,21 @@
 package me.Johnny.PersistentTeamDataType.Events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.scoreboard.Team;
 
 import me.Johnny.PersistentTeamDataType.Main;
+import me.Johnny.PersistentTeamDataType.SkinGrabber;
 import me.Johnny.PersistentTeamDataType.TeamDataType;
 import me.Johnny.PersistentTeamDataType.Models.TeamType;
 
@@ -17,24 +25,88 @@ public class Events implements Listener {
 		//Grab player
 		Player player = event.getPlayer();
 		
+		// Create score board for player
+		
+		
 		NamespacedKey key = new NamespacedKey(Main.getPlugin(Main.class), "teamType");
 		
+		// When player joins, the "team" that they default to is neutral
 		if (!player.getPersistentDataContainer().has(key, new TeamDataType())) {
 			player.getPersistentDataContainer().set(key, new TeamDataType(), TeamType.NEUTRAL);
 		}
 		
+		/* How this team assignment code works
+		 * Get the player's persistence data container for 'teamType'
+		 * If it is of a certain type, them change their skin accordingly
+		 * Go through all players and generate a scoreboard that
+		 * reflects their team type => createScoreboard(Player player, String team)
+		 * */
+		
 		if (player.getPersistentDataContainer().get(key, new TeamDataType()).equals(TeamType.BLUE)) {
 			player.sendMessage(ChatColor.BLUE + "Violets are blue...");
+			//CHANGE CHARACTER SKIN
+			SkinGrabber.changeSkin(player, "BLUE");
+			
+//			createScoreboard(player, "BLUE");
 		}
 		else if (player.getPersistentDataContainer().get(key, new TeamDataType()).equals(TeamType.RED)) {
 			player.sendMessage(ChatColor.RED + "Roses are red...");
+			//CHANGE CHARACTER SKIN
+			SkinGrabber.changeSkin(player, "RED");
+//			createScoreboard(player, "RED");
 		}
 		else if (player.getPersistentDataContainer().get(key, new TeamDataType()).equals(TeamType.NEUTRAL)) {
 			player.sendMessage(ChatColor.WHITE + "Stuck in the middle...");
+			//CHANGE CHARACTER SKIN
+			SkinGrabber.changeSkin(player, "WHITE");
+//			createScoreboard(player, "NEUTRAL");
 		}
 		else {
-			player.sendMessage("You are not a part of a team yet. Use the command /teams to see a list of joinable teams");
+			player.sendMessage("Don't know why you're here. You should have defaulted to a NEUTRAL TEAM");
 		}
+		
+		// Create a scoreboard for each player based on their team type
+		for (Player p: Bukkit.getServer().getOnlinePlayers()) {
+			String teamColor = p.getPersistentDataContainer().get(key, new TeamDataType()).toString();
+			createScoreboard(p, teamColor);
+		}
+		
+	}
+	public void createScoreboard(Player player, String displayName) {
+		ScoreboardManager manager = Bukkit.getScoreboardManager();
+		Scoreboard board = manager.getNewScoreboard();
+		Objective objective = board.registerNewObjective("NAME", "dummy");
+		objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+		
+		Team red = board.registerNewTeam("RED");
+		Team blue = board.registerNewTeam("BLUE");
+		Team neutral= board.registerNewTeam("NEUTRAL");
+		
+		red.setPrefix(ChatColor.RED + "[RED]" + ChatColor.WHITE);
+		blue.setPrefix(ChatColor.BLUE+ "[BLUE]" + ChatColor.WHITE);
+		neutral.setPrefix(ChatColor.WHITE+ "[NEUTRAL]" + ChatColor.WHITE);
+		
+		Score score = objective.getScore("Players:");
+		score.setScore(Bukkit.getOnlinePlayers().size());
+		
+		player.setScoreboard(board);
+		
+		if (displayName.equalsIgnoreCase("blue")) {
+			objective.setDisplayName(ChatColor.BLUE + "" + ChatColor.BOLD + displayName);
+			blue.addPlayer(player);
+			SkinGrabber.changeSkin(player, "BLUE");
+		}	
+		else if (displayName.equalsIgnoreCase("red")) {
+			objective.setDisplayName(ChatColor.RED + "" + ChatColor.BOLD + displayName);
+			red.addPlayer(player);
+			SkinGrabber.changeSkin(player, "RED");
+		}
+		else if (displayName.equalsIgnoreCase("neutral")) {
+			objective.setDisplayName(ChatColor.WHITE + "" + ChatColor.BOLD + displayName);
+			neutral.addPlayer(player);
+			SkinGrabber.changeSkin(player, "NEUTRAL");
+		}
+		
 		
 	}
 }
--- PersistentTeamDataType/src/me/Johnny/PersistentTeamDataType/TeamCommands.java
+++ PersistentTeamDataType/src/me/Johnny/PersistentTeamDataType/TeamCommands.java
@@ -6,13 +6,22 @@ import java.util.List;
 
 //import org.bukkit.ChatColor;
 import net.md_5.bungee.api.ChatColor;
+
+import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.scoreboard.Team;
 
+import me.Johnny.PersistentTeamDataType.Events.Events;
 import me.Johnny.PersistentTeamDataType.Models.TeamType;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
@@ -36,9 +45,24 @@ public class TeamCommands implements CommandExecutor {
 			cc.add(ChatColor.RED);
 			cc.add(ChatColor.WHITE);
 			
+			Events e = new Events();
+			
 			// Test command for sanity purposes
 			if (label.equalsIgnoreCase("test")) {
 				player.sendMessage("Could you be the one?");
+			}
+			
+			// Feature that allows for users to hover/click on the team they would like to join
+			if (label.equalsIgnoreCase("teams")) {
+				player.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Select a Team Shown Below:");
+				for (String teamName: map.keySet()) {
+					TextComponent message = new TextComponent(teamName);
+					message.setColor(cc.get(map.get(teamName)));
+					message.setBold(true);
+					message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/assign " + teamName));
+					message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click here to join the " + teamName + ChatColor.WHITE + " team")));
+					player.spigot().sendMessage(message);
+				}
 			}
 			
 			// Team assignment code
@@ -47,14 +71,20 @@ public class TeamCommands implements CommandExecutor {
 					// JOIN THE BLUE TEAM
 					if (args[0].equalsIgnoreCase("blue")) {
 						teamAssignment(player, ChatColor.BLUE, TeamType.BLUE);
+						e.createScoreboard(player, "BLUE");
+//						createScoreboard(player, "BLUE");
 					}
 					// JOIN THE RED TEAM
 					else if (args[0].equalsIgnoreCase("red")) {
 						teamAssignment(player, ChatColor.RED, TeamType.RED);
+						e.createScoreboard(player, "RED");
+//						createScoreboard(player, "RED");
 					}
 					// JOIN THE NEUTRAL TEAM
 					else if (args[0].equalsIgnoreCase("neutral")) {
 						teamAssignment(player, ChatColor.WHITE, TeamType.NEUTRAL);
+						e.createScoreboard(player, "NEUTRAL");
+//						createScoreboard(player, "NEUTRAL");
 					}
 					else {
 						player.sendMessage(ChatColor.WHITE + args[0] + " is an invalid team name...");
@@ -71,18 +101,7 @@ public class TeamCommands implements CommandExecutor {
 				PersistentDataContainer data = player.getPersistentDataContainer();
 				NamespacedKey keyValue = new NamespacedKey(Main.getPlugin(), "teamType");
 				String team = data.get(keyValue, new TeamDataType()).toString();
-				player.sendMessage(ChatColor.GRAY + "You are on the : " + cc.get(map.get(team)) + data.get(keyValue, new TeamDataType()) + ChatColor.WHITE +" team!");
-			}
-			// Feature that allows for users to hover/click on the team they would like to join
-			if (label.equalsIgnoreCase("teams")) {
-				for (String teamName: map.keySet()) {
-					TextComponent message = new TextComponent(teamName);
-					message.setColor(cc.get(map.get(teamName)));
-					message.setBold(true);
-					message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/assign " + teamName));
-					message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click here to join the " + teamName + ChatColor.WHITE + " team")));
-					player.spigot().sendMessage(message);
-				}
+				player.sendMessage(ChatColor.GRAY + "You are on the : " + cc.get(map.get(team)) + ChatColor.BOLD + data.get(keyValue, new TeamDataType()) + ChatColor.WHITE +" team!");
 			}
 		}
 		return false;
@@ -93,10 +112,42 @@ public class TeamCommands implements CommandExecutor {
 		NamespacedKey key = new NamespacedKey(Main.getPlugin(Main.class), "teamType");
 		if (data.has(key, new TeamDataType())) {
 			data.set(key, new TeamDataType(), team);
-			player.sendMessage(ChatColor.GRAY + "You are on the : " + color + data.get(new NamespacedKey(Main.getPlugin(), "teamType"), new TeamDataType()) + " team!");
+			player.sendMessage(ChatColor.GRAY + "You are now on the : " + color + ChatColor.BOLD + data.get(new NamespacedKey(Main.getPlugin(), "teamType"), new TeamDataType()) + ChatColor.RESET + " team!");
 		}
 		else {
 			player.sendMessage(ChatColor.GREEN + "Uh oh");
 		}
 	}
+	
+//	public void updateTeamNameScoreboard(Player player, String displayName) {
+//		ScoreboardManager manager = Bukkit.getScoreboardManager();
+//		Scoreboard board = manager.getNewScoreboard();
+//		Objective objective = board.registerNewObjective("NAME", "dummy");
+//		objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+//		
+//		Team red = board.registerNewTeam("RED");
+//		Team blue = board.registerNewTeam("BLUE");
+//		Team neutral= board.registerNewTeam("NEUTRAL");
+//		
+//		red.setPrefix(ChatColor.RED + "[RED] " + ChatColor.WHITE);
+//		blue.setPrefix(ChatColor.BLUE+ "[BLUE] " + ChatColor.WHITE);
+//		neutral.setPrefix(ChatColor.WHITE+ "[NEUTRAL] " + ChatColor.WHITE);
+//		
+//		Score score = objective.getScore("Players:");
+//		score.setScore(Bukkit.getOnlinePlayers().size());
+//		player.setScoreboard(board);
+//		
+//		if (displayName.equalsIgnoreCase("blue")) {
+//			objective.setDisplayName(ChatColor.BLUE + "AZUL");
+//			blue.addPlayer(player);
+//		}
+//		else if (displayName.equalsIgnoreCase("red")) {
+//			objective.setDisplayName(ChatColor.RED + displayName.toUpperCase());
+//			red.addPlayer(player);
+//		}
+//		else if (displayName.equalsIgnoreCase("neutral")) {
+//			objective.setDisplayName(ChatColor.WHITE + displayName.toUpperCase());
+//			neutral.addPlayer(player);
+//		}
+//	}
 }
